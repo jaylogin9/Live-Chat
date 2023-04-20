d5f5c07fd734ceee308b02f40f1636a3eb9c8b4d
jaylogin9 <jaylogin9@gmail.com>
2023-04-20 23:06:46 +0530

    check

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const compression = require('compression');
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+// compress all responses
+app.use(compression());
+
+http.listen(PORT, () => {
+    console.log(`Listening on port http://localhost:${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        console.info(`Socket ${socket.id} says: "${msg}"`);
+        socket.broadcast.emit('message', msg)
+    })
+    socket.on('disconnect',()=> {
+        console.log('A user disconnected');
+    })
+
+})
